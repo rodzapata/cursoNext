78e37129d27bbc6dea11d7aa5344c1a922d32ab5
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2025-12-12 16:23:39 -0500

    styles

--- projnext1p/src/app/post/[postId]/page.jsx
+++ projnext1p/src/app/post/[postId]/page.jsx
@@ -1,31 +1,30 @@
 import Post from "../page";
 import { Suspense } from "react";
-import SkeletonPosts from "@/app/components/SkeletonPosts";
 
 async function loadPost(id) {
-    const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    const data = await resp.json()
-    return data;
+  const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await resp.json()
+  return data;
 }
 
 
 export default async function Page({ params }) {
-    const { postId } = await params;
+  const { postId } = await params;
 
-    const post = await loadPost(postId);
+  const post = await loadPost(postId);
 
-    return (
-        <>
-            <h2>post {postId} - {post.title}</h2>
-            <p>{post.body}</p>
+  return (
+    <>
+      <h2>post {postId} - {post.title}</h2>
+      <p>{post.body}</p>
 
-            <h1>Otras publicaciones</h1>
-            <hr />
-            <Suspense fallback={<SkeletonPosts />}>
-                <Post />
-            </Suspense>
+      <h1>Otras publicaciones</h1>
+      <hr />
+      <Suspense fallback={<h3>cargnado otras publicaciones ....</h3>}>
+        <Post />
+      </Suspense>
 
-        </>
-    )
+    </>
+  )
 }
 
