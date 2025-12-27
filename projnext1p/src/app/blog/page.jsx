122ab907dd4ad206ecde8d16fa49c42866bc57f3
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2025-12-27 12:08:00 -0500

    config TailwindCss in NextJS!

--- projnext1p/src/app/blog/page.jsx
+++ projnext1p/src/app/blog/page.jsx
@@ -1,7 +1,7 @@
 export default function Blog(){
     return(
         <>
-            <h1>Blog Page</h1>
+            <h1 className="text-amber-50 bg-amber-500">Blog Page</h1>
         </>
     )
 }
